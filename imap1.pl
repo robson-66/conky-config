80fce29cde3dc1570dd94d69d088ae5cfe699a58
robson-66 <robson75@linux.pl>
2022-02-18 12:40:26 +0100

    update

---
+++ imap1.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+ 
+# by gxmsgx
+# description: get the count of unread messages on imap
+
+use strict;
+use Mail::IMAPClient;
+use IO::Socket::SSL;
+
+my $username = 'your_mail'; 
+my $password = 'password'; 
+
+my $socket = IO::Socket::SSL->new(
+  PeerAddr => 'mail.linux.pl',
+  PeerPort => 993
+ )
+ or die "socket(): $@";
+ 
+my $client = Mail::IMAPClient->new(
+  Socket   => $socket,
+  User     => $username,
+  Password => $password,
+ )
+ or die "new(): $@";
+ 
+if ($client->IsAuthenticated()) {
+  my $msgct;
+
+  $client->select("INBOX");
+  $msgct = $client->unseen_count||'0';
+  print "$msgct\n";
+}
+
+$client->logout();
